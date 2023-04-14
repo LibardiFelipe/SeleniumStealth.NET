00f63b78400287fa21e3b95eb040cd53e3602fb9
Felipe Libardi Gonçalves <flibardig@gmail.com>
2023-04-14 12:18:44 -0300

    Adicionar arquivos de projeto.

---
+++ SeleniumStealth.NET.Infrastructure.Build/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniumStealth.NET.Infrastructure.Build
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SeleniumStealth.NET.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace SeleniumStealth.NET.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            Assert.True(true);
+    }
+}

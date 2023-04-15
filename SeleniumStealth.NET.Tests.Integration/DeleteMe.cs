0733d2955406978abb1c7b69cc833aef617bd902
Felipe Libardi Gonçalves <flibardig@gmail.com>
2023-04-15 15:47:13 -0300

    FEATURE: Added extension to simulate mouse movement over certain elements and its integration test.

---
+++ SeleniumStealth.NET.Tests.Integration/DeleteMe.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace SeleniumStealth.NET.Tests.Integration
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            Assert.True(condition: true);
+    }
+}

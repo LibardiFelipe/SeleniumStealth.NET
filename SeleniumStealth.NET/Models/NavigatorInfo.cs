3afb5c1549ad42b663926a4507e0b8ca468c43ef
Felipe Libardi Gonçalves <flibardig@gmail.com>
2023-04-17 21:35:22 -0300

    FEATURE: Added more vendors, userAgents and webGl when generating random informations about the navigator.

--- SeleniumStealth.NET/Models/NavigatorInfo.cs
+++ SeleniumStealth.NET/Models/NavigatorInfo.cs
@@ -20,7 +20,9 @@ namespace SeleniumStealth.NET.Models
 
         private static string GetRandomNavigatorVendor()
         {
-            string[] vendors = { "Google Inc.", "Microsoft Corporation", "Mozilla Foundation" };
+            string[] vendors = { "Google Inc.", "Microsoft Corporation",
+                "Mozilla Foundation", "Apple Inc.", "Opera Software" };
+
             return vendors[_rnd.Next(vendors.Length)];
         }
 
@@ -30,6 +32,8 @@ namespace SeleniumStealth.NET.Models
             {
                 "Google Inc." => $"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{_rnd.Next(80, 100)}.0.{_rnd.Next(1000, 9999)}.{_rnd.Next(1, 200)} Safari/537.36",
                 "Microsoft Corporation" => $"Mozilla/5.0 (Windows NT 10.0; Win64; x64; Trident/7.0; rv:{_rnd.Next(10, 13)}.0) like Gecko",
+                "Apple Inc." => $"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_{_rnd.Next(12, 16)}_{_rnd.Next(1, 9)}) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/{_rnd.Next(11, 15)}.0 Safari/605.1.15",
+                "Opera Software" => $"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{_rnd.Next(60, 80)}.0.{_rnd.Next(1000, 9999)}.{_rnd.Next(1, 200)} Safari/537.36 OPR/{_rnd.Next(40, 60)}.0.{_rnd.Next(1000, 9999)}.{_rnd.Next(1, 200)}",
                 _ => $"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:{_rnd.Next(50, 100)}.0) Gecko/20100101 Firefox/{_rnd.Next(70, 100)}.0",
             };
         }
@@ -38,10 +42,13 @@ namespace SeleniumStealth.NET.Models
         {
             string webGLRenderer = webGLVendor switch
             {
-                "Google Inc." => "ANGLE (Intel(R) HD Graphics 630 Direct3D11 vs_5_0 ps_5_0)",
+                "Google Inc." => $"ANGLE (Intel(R) HD Graphics {_rnd.Next(600, 700)} Direct3D11 vs_5_0 ps_5_0)",
                 "Microsoft Corporation" => "Microsoft Basic Render Driver",
-                _ => "ANGLE (AMD Radeon R7 370 Direct3D11 vs_5_0 ps_5_0)",
+                "Apple Inc." => $"Apple GPU {_rnd.Next(1, 5)}",
+                "Opera Software" => $"ANGLE (NVIDIA GeForce {_rnd.Next(900, 1000)} GT {_rnd.Next(1, 10)} Direct3D11 vs_5_0 ps_5_0)",
+                _ => $"ANGLE (AMD Radeon R{_rnd.Next(5, 9)} {_rnd.Next(300, 400)} Direct3D11 vs_5_0 ps_5_0)"
             };
+
             return (webGLVendor, webGLRenderer);
         }
     }
